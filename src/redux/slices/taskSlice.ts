3e167aaf5f40515d19c4ee6e4d9f0c5548b54254
Aviv Avisror <aviva@restigo.co.il>
2024-10-30 10:48:10 +0200

    implemented filters

--- src/redux/slices/taskSlice.ts
+++ src/redux/slices/taskSlice.ts
@@ -1,5 +1,4 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { FiltersType } from "../../constants/TypesConstant";
 
 import axiosApi from "../../constants/axiosApi";
 import { AxiosError, isAxiosError } from "axios";
